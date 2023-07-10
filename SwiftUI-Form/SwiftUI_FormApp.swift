9c10357d71ff77589e029cf2922a833e50da567a
CanerCagri <canerrcagrii@gmail.com>
2023-07-10 17:54:37 +0300

    Initial Commit

---
+++ SwiftUI-Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI-Form
+//
+//  Created by Caner Çağrı on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
